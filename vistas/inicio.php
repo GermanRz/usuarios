498418e09c83ffe6a9465f6dc75f24ac3bf998f5
GOMA-DEV <p-goma@hotmail.com>
2026-03-06 19:19:23 -0500

    proceso de mostrar y listar los usuarios en el inicio

--- vistas/inicio.php
+++ vistas/inicio.php
@@ -1,9 +1,7 @@
 <?php
-    $controlador = new Controlador();
-    $resultado = $controlador->index();
+    $controlador = new ControladorUsuarios();
+    $resultado = $controlador->listar();
 ?>
-
-
 
 <table border="1">
     <thead>
@@ -18,76 +16,28 @@
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>1</td>
-            <td>Carlos</td>
-            <td>Ramirez</td>
-            <td>100000001</td>
-            <td>cramirez</td>
-            <td>123456</td>
-            <td>
-                <a href="?cargar=consultar">Consultar</a> |
-                <a href="?cargar=editar">Editar</a> |
-                <a href="?cargar=eliminar">Eliminar</a>
-            </td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Ana</td>
-            <td>Gomez</td>
-            <td>100000002</td>
-            <td>agomez</td>
-            <td>123456</td>
-            <td>
-                <a href="?cargar=consultar">Consultar</a> |
-                <a href="?cargar=editar">Editar</a> |
-                <a href="?cargar=eliminar">Eliminar</a>
-            </td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>Luis</td>
-            <td>Martinez</td>
-            <td>100000003</td>
-            <td>lmartinez</td>
-            <td>123456</td>
-            
-             <td>
-                <a href="?cargar=consultar">Consultar</a> 
-                <a href="?cargar=editar">Editar</a> 
-                <a href="?cargar=eliminar">Eliminar</a>
-            </td>
-            
-        </tr>
-        <tr>
-            <td>4</td>A
-            <td>Laura</td>
-            <td>Fernandez</td>
-            <td>100000004</td>
-            <td>lfernandez</td>
-            <td>123456</td>
-            
-             <td>
-                <a href="?cargar=consultar">Consultar</a> |
-                <a href="?cargar=editar">Editar</a> |
-                <a href="?cargar=eliminar">Eliminar</a>
-            
-            </td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>Jorge</td>
-            <td>Castro</td>
-            <td>100000005</td>
-            <td>jcastro</td>
-            <td>123456</td>
-            
-             <td>
-                <a href="?cargar=consultar">Consultar</a> |
-                <a href="?cargar=editar">Editar</a> |
-                <a href="?cargar=eliminar">Eliminar</a>
-            
-            </td>
-        </tr>
-    </tbody>
-</table>
+        <?php
+            while($fila = mysqli_fetch_array($resultado)){
+                echo "<tr>";
+                echo "<td>".$fila["idUsuario"]."</td>";
+                echo "<td>".$fila["nombres"]."</td>";
+                echo "<td>".$fila["apellidos"]."</td>";
+                echo "<td>".$fila["cedula"]."</td>";
+                echo "<td>".$fila["usuario"]."</td>";
+                echo "<td>".$fila["password"]."</td>";
+                echo "<td>
+                <a href='?cargar=consultar&id=".$fila["idUsuario"]."'>Consultar</a>
+                <a href='?cargar=editar&id=".$fila["idUsuario"]."'>Editar</a>
+                <a href='?cargar=eliminar&id=".$fila["idUsuario"]."'>Eliminar</a>";
+
+                echo "</tr>";
+            }
+        ?>
+                <!-- <a href='?cargar=consultar'>Consultar</a> |
+                <a href='?cargar=editar'>Editar</a> |
+                <a href='?cargar=eliminar'>Eliminar</a>
+            </td> -->
+
+
+
+</tbody>
